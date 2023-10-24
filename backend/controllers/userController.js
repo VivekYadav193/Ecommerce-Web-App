de8950da0b30c53e56f70b1861ba329d73696c80
VivekYadav193 <vivekash03@gmail.com>
2023-10-24 09:24:21 +0530

    commit 3

---
+++ backend/controllers/userController.js
@@ -0,0 +1,52 @@
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+
+const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
+
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "this is a sample id",
+      url: "profilepic.jpg",
+    },
+  });
+
+ sendToken(user, 200, res);
+
+});
+
+//login user
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  
+    const { email, password } = req.body;
+  //check if email and password is entered by user
+  
+  if (!email || !password) 
+  {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+  //finding user in database
+  const user = await User.findOne({ email }).select("+password");
+  
+  if (!user) 
+  {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+  //check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+  
+  if (!isPasswordMatched) 
+  {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+    sendToken(user, 200, res);
+
+
+});
