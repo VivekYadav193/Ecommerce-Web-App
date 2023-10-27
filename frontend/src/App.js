f5b6c1339b38dd9100ad426c6e35f55dd723644c
VivekYadav193 <vivekash03@gmail.com>
2023-10-27 17:37:29 +0530

    commit 8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./component/layout/Header.js";
+import webFont from "webfontloader";
+import React from "react";
+
+
+
+
 
 function App() {
-  return (
+
+  React.useEffect(() => {
+    webFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }
+  , []);
   
 
-    <>
-    
-    
-    
-    </>
 
-
+  return (
+    <Router>
+      <Header />
+    </Router>
   );
 }
 
